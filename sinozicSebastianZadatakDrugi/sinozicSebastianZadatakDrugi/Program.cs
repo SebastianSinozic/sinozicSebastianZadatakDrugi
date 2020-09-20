fbc74172f177e2371b7d2193c41146ffa58b0583
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-09-20 21:42:49 +0200

    Add files via upload

---
+++ sinozicSebastianZadatakDrugi/sinozicSebastianZadatakDrugi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sinozicSebastianZadatakDrugi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sek;
+            int min;
+            int s;
+
+
+            Console.WriteLine("Upisi sekunde");
+            sek = Convert.ToInt32(Console.ReadLine());
+            min = sek / 60;
+            s = sek % 60;
+            Console.WriteLine("{0} minuta i {1} sekundi", min, s);
+            Console.ReadKey();
+
+
+        }
+    }
+}
